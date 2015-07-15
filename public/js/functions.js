4dbda6052da18ec0cccc735a25e3f6b0d1806e64
Luca Boieru <luca.boieru@yahoo.com>
2015-07-15 20:53:33 +0300

    files.

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,45 +1,8 @@
-$(document).ready(function (){
+var Portfolio = angular.module("Portfolio", []);
 
-	$(".btn").on("click", function () {
-
-		makeAjaxRequest({
-			operation: "/@/getExample",
-			data: {}
-		}, function (err, data) {
-			console.log(data);
-		});
-	});
-});
-
-function makeAjaxRequest (ajaxObj, callback) {
-
-    $.ajax({
-        url: ajaxObj.operation,
-        type: "POST",
-        data: ajaxObj.data,
-        crossDomain: true,
-        error: function(jqXHR, exception) {
-            if (jqXHR.status === 0) {
-                callback('Not connect. Verify Network.' + jqXHR.responseText);
-            } else if (jqXHR.status == 404) {
-                callback('Requested page not found. [404].' + jqXHR.responseText);
-            } else if (jqXHR.status == 500) {
-                callback('Internal Server Error [500].' + jqXHR.responseText);
-            } else if (exception === 'parsererror') {
-                callback('Requested JSON parse failed.');
-            } else if (exception === 'timeout') {
-                callback('Time out error.');
-            } else if (exception === 'abort') {
-                callback('Ajax request aborted.');
-            } else {
-                callback('Uncaught Error.\n' + jqXHR.responseText);
-            }
-        },
-        success: function (data) {
-
-            // response is a string, needs to be parsed
-            var data = JSON.parse(data);
-            callback(null, data);
-        }
+Portfolio.controller("portfolioCtrl", function ($scope, $http) {
+    $http.get("/projects.json").success(function (projects) {
+        $scope.projects = projects;
     });
-}
+    $scope.limit = 10;
+});
