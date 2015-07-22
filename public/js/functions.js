937c34393b4561fddf45cb133eea3d0ae83d6dc6
Luca Boieru <luca.boieru@yahoo.com>
2015-07-22 18:57:33 +0300

    New grid view.

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,4 +1,4 @@
-$.embedly.defaults.key = '7be05f589d08461494612a6cc5dcd61f';
+$.embedly.defaults.key = '0291b99310294191a17b5307ce1db307';
 
 var Portfolio = angular.module("Portfolio", []);
 
@@ -8,61 +8,48 @@ Portfolio.controller("portfolioCtrl", function ($scope, $http) {
         $scope.addImages(0);
     });
 
-    $scope.skip = 0;
-    $scope.limit = 6;
 
     $scope.addImages = function (skip) {
 
         $(".spinner").show();
         $(".loadMore").hide();
 
-        var heightLimit = 1200;
+        for (var i = 0; i < $scope.projects.length; ++ i) {
+            (function (i) {
+                var url = "http://104.155.46.85:9999/projects/" + $scope.projects[i];
 
-        function next(i, prevHeight) {
+                $.embedly.extract(url).progress(function(obj) {
+                    // Grab images and create the colors.
+                    var img = obj.images[0];
+                    var color = "rgb(" + img.colors[0].color[0] + "," + img.colors[0].color[1] + "," + img.colors[0].color[2] + ")";
 
-            if (i >= $scope.projects.length) {
-                $(".spinner").hide();
-                return;
-            }
-
-            if (prevHeight > heightLimit) {
-                $(".spinner").hide();
-                $(".loadMore").show();
-                return;
-            }
-
-            $scope.skip ++;
-
-            var url = "http://104.155.46.85:9999/projects/" + $scope.projects[i];
-
-            $.embedly.extract(url).progress(function(obj) {
-                // Grab images and create the colors.
-                var img = obj.images[0];
-                var color = "rgb(" + img.colors[0].color[0] + "," + img.colors[0].color[1] + "," + img.colors[0].color[2] + ")";
-
-                // Display the image.
-                displayImage(i, img, color, function (height) {
-                    next(i + 2, height + prevHeight);
+                    // Display the image.
+                    displayImage(i, img, color, url);
                 });
-            });
+            })(i);
         }
-
-        next(skip, 0);
-        next(skip + 1, 0);
     };
 });
 
-var displayImage = function(i, img, color, callback) {
- 
-    var width = $(".container1").width(),
-        height = Math.ceil(img.height * (width / img.width));
+$(document).ready(function () {
+    $('#zoom').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var url = button.data('url');
+        var modal = $(this);
+        modal.find('img').attr("src", url);
+    });
+});
 
-    callback(height);
+var displayImage = function(i, img, color, url) {
+ 
+    var width = $(".container0").width(),
+        height = Math.ceil(img.height * (width / img.width));
     
-    var $temp = $('<div class="backgroundBlock"></div>');
+    var $temp = $('<div data-url="' + url + '" class="backgroundBlock" data-toggle="modal" data-target="#zoom"></div>');
 
     // Add the block to the results section.
-    $((i % 2 ? ".container2" : ".container1")).append($temp);
+    var container = ".container" + (i % 4);
+    $(container).append($temp);
 
     // Build the actually placeholder.
     $temp.css({
